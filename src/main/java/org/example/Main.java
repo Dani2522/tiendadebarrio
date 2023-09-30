6b0b0fdf1947df5b85bca5d683b72652b7c0654e
danielamoreno <lauritamobu@gmail.com>
2023-09-29 23:30:28 -0500

    semodifica las variables del inventario

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,11 +3,25 @@ import jdk.dynalink.beans.StaticClass;
 
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
 
+        String name = "Leche Entera";
+        String description = "Bolsa x 1.100 ml";
+        String catogary = "Lacteos";
+        String Label = "Leches";
+        double Price = 3.100;
+        String Url = "F2\n" +
+                "\n" +
+                "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg";
+
+        System.out.println("Nombre: " + name);
     }
+
 }
+
+
+
+
 
 
