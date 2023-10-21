6a59a17360fcfca635b9546e26bd602b1599953f
danielamoreno <lauritamobu@gmail.com>
2023-10-21 14:49:53 -0500

    se agrega menu

---
+++ src/main/java/org/example/menu.java
@@ -0,0 +1,55 @@
+package org.example;
+import java.util.Scanner;
+
+public class menu {
+
+    private void handleUserChoice(int choice) {
+        switch (choice) {
+            case 1 -> addProduct();
+            case 2 -> removeProduct();
+            case 3 -> updateProduct();
+            case 4 -> getAllProducts();
+            case 5 -> System.out.println("Saliendo...");
+            default -> System.out.println("Opción invalida. Por favor intenta de nuevo.");
+        }
+
+    }
+
+    private void displayMenu() {
+
+        System.out.println(
+                """
+                        |o|                         /////////////\\\\\\
+                        |o|                        (((((((((((((   \\\\\\
+                        |o|                        ))) ~~      ~~   (((
+                        |o|                        ((( (*)     (*)  )))
+                        |o|                        )))     <        (((\s
+                        |o|                        ((( '\\______/`   )))\s
+                        |o|                        )))\\___________/(((\s
+                        |o|                        (((   _)  (_    )))\s\s
+                        |o|                              /\\__/\\""");
+        System.out.println("±----------------------------------------±");
+        System.out.println("|   Administrador Mi Tienda de Barrio    |");
+        System.out.println("±----------------------------------------±");
+        System.out.println("1. Agregar producto                      |");
+        System.out.println("2. Eliminar producto                     |");
+        System.out.println("3. Actualizar producto                   |");
+        System.out.println("4. Ver todos los productos               |");
+        System.out.println("5. Salir                                  |");
+        System.out.println("±----------------------------------------±");
+        System.out.print("   Ingresa tu opción:    (1 - 5)  ");
+    }
+
+    public void runMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do {
+            displayMenu();
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+            handleUserChoice(choice);
+        } while (choice != 8);
+    }
+
+
+}
