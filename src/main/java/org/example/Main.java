990bbd676f15b690f07cf5d8512b71962542a2cd
danielamoreno <lauritamobu@gmail.com>
2023-10-21 16:48:06 -0500

    se agregan instancias

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,22 @@
 package org.example;
-import jdk.dynalink.beans.StaticClass;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        String name;
-        String description;
-        String catogary;
-        String Label;
-        double Price;
-
-
-
         menu m = new menu();
         m.runMenu();
+
+        producto prod1 = new producto(1, "Leche Entera Alpina", "Bolsa x 1.100 ml", "Lacteo/Huevos y refrigerados", "Leches", 3100.00, "https://www.eurosupermercados.com/eurosupermercado2020//contenidos/images/ecommerce_productos/7702001041404.jpg" );
+        producto prod2 = new producto(2, "Leche Alpina Deslactosada ", "Bolsa x 1.100 ml", "Lacteo/Huevos y refrigerados", "Leches", 3400.00, "https://cdn.derematazo.com/wp-content/uploads/2019/08/Leche-Alpina-Deslactosada-Bolsa-x-1100ml-23445.jpg" );
+
+        System.out.println("Instancias de producto:\n");
+        System.out.print(prod1);
+        System.out.println("\n");
+        System.out.print(prod2);
+
     }
 
 }
-
 
 
 
--- src/main/java/org/example/producto.java
+++ src/main/java/org/example/producto.java
@@ -1,5 +1,27 @@
 package org.example;
 
 public class producto {
+    Integer id;
+    String name;
+    String description;
+    String category;
+    String label;
+    double price;
+    String photoUrl;
+
+    producto(Integer _id, String _name, String _description, String _category, String _label, double _price, String _photoUrl){
+        this.id = _id;
+        this.name = _name;
+        this.description = _description;
+        this.category = _category;
+        this.label = _label;
+        this.price = _price;
+        this.photoUrl = _photoUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "\nid: "+id+"\nname: "+name+"\ndescription: "+description+"\ncategory: "+category+"\nlabel: "+label+"\nprice: "+price+"\nPhoto: "+photoUrl;
+    }
 
 }
